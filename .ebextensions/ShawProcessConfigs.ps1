9f02b20c1a836cc0ee95fcc6f19f600f5a4adeb9
Ronnie Miller <ronnie.miller@shawinc.com>
2020-02-19 22:33:43 +0000

    Edited ShawProcessConfigs.ps1

--- .ebextensions/ShawProcessConfigs.ps1
+++ .ebextensions/ShawProcessConfigs.ps1
@@ -32,7 +32,7 @@
 #  <add key="ida:RedirectUri" value="https://localhost:44380/" />
 #</appSettings>
 
-$configPath = "C:\inetpub\wwwroot\_ConfigFiles\"
+$configPath = "_ConfigFiles\"
 $connectionConfig = "ConnectionStrings.config"
 
 $rdsDB = (Get-ChildItem Env:RDS_DB_NAME).Value
